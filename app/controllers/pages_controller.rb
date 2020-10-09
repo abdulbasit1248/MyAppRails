4f5be9678d008172b056f9a078bdd6ba37280aaa
abdulbasit1248 <abdul.basit@thedevden.co>
2020-10-09 11:23:46 +0500

    Added gem httparty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @aaa = 'basit'
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
  root 'pages#home'
  get 'about', to: 'pages#about'
+ get "sessions/destroy", as: :logout 
+#  get ‘/stories’, to: redirect(‘/articles’)
+#  resources :pages
+#  resources :users, only: [:index, :new, :create]
+ namespace :admin do
+  resources :articles, :comments
+ end
 end
